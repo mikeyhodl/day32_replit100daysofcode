c09ffd4240c3871906e2326773e041432076fe49
mikey <mike.owino40@yahoo.com>
2022-10-13 20:10:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+
+# timetable = ["Computer Science", "Math", "English", "Art", "Sport"]
+# print(timetable)
+
+
+# timetable = ["Computer Science", "Math", "English", "Art", "Sport"]
+# print(timetable)
+
+
+# timetable = ["Computer Science", "Math", "English", "Art", "Sport"]
+# print(timetable[1])
+
+# timetable = ["Computer Science", "Math", "English", "Art", "Sport"]
+# print(timetable[0])
+# print(timetable[1])
+# print(timetable[2])
+# print(timetable[3])
+# print(timetable[4])
+
+# timetable[4]= "Watch TV"
+# print(timetable[0])
+# print(timetable[1])
+# print(timetable[2])
+# print(timetable[3])
+# print(timetable[4])
+
+# timetable = ["Computer Science", "Math", "English", "Art", "Watch TV"]
+# for n in timetable:
+#   print(n)
+
+# colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Violet"]
+# print(f"The first color is {colors[0]}")
+
+# colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Violet"]
+# print(f"The first color is {colors[5]}")
+
+# grocery_list = ["bananas", "bread", "milk", "eggs", "juice", "cheese"]
+# print(f"The first grocery item to buy is {grocery_list[0]}.")
+
+import random
+greetings = ["Hello there!", "Konnichiwa", "Guten Tag!", "Bore Da!", "Bonjour", "Zdravstvuyte", "Olá", "Anyoung haseyo", "Anyoung haseyo"]
+index = random.randint(0,8)
+print(greetings[index])
+                             
+# </details>
